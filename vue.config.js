f4a7a5abe648add9ffd7e6d0565c16fdd34de571
Siva <Siva@10016>
2023-02-03 22:31:21 +0530

    config-to-github-name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: 'https://test.bhyam.com/',
+  base: '/bhyam-test/',
 })
